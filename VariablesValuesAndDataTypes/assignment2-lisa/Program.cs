e4306f655f942cba672fde045b842f05d2097219
starlite998 <starlite998@hotmail.com>
2014-05-21 15:23:37 -0600

    Assignment2

---
+++ VariablesValuesAndDataTypes/assignment2-lisa/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2_lisa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool attendingSchool = false;
+            bool workingFullTime = true;
+
+            string name = "Lisa";
+            string city = "Edmonton";
+
+            int yearsCollege = 1;
+            int yearsUniversity = 4;
+
+            int totalPostSecondaryYears = yearsCollege + yearsUniversity;
+
+            bool attendingSchoolandWorking = attendingSchool && workingFullTime;
+            bool moreThan4YearsPostSecondary = totalPostSecondaryYears > 4;
+            bool hardWorkingOrSmart = true;
+
+            Console.WriteLine("Name " + name + " from  " + city);
+            Console.WriteLine("Attending School and Working: " + attendingSchoolandWorking);
+            Console.WriteLine("More than 4 years of Post Secondary: " + moreThan4YearsPostSecondary);
+            Console.WriteLine("Hard Working or Smart: " + hardWorkingOrSmart);
+
+        }
+    }
+}
+ 
